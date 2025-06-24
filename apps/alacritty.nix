fc2b42f32aa003894825a39f189c22d8b595bebf
Bamidev <bamidev@pm.me>
2025-06-24 22:56:11 +0200

    Improve alacritty config

--- apps/alacritty.nix
+++ apps/alacritty.nix
@@ -1,9 +1,14 @@
 {
-  enable = true;
+  programs.alacritty.enable = true;
 
-  settings = {
-    font = {
-      builtin_box_drawing = true;
-	};
-  };
+  home.file.".config/alacritty/alacritty.toml".text = '' 
+    [font]
+    builtin_box_drawing = true
+    size = 12
+
+    [[hints.enabled]]
+    command = { program = "vi", args = [ "+" ] }
+    mouse = { enabled = true }
+    regex = "[^ ]+\\.rs:\\d+:\\d+"
+  '';
 }
--- users/defaults.nix
+++ users/defaults.nix
@@ -2,37 +2,38 @@
   home.stateVersion = "24.11";
 
   imports = [
+    ../apps/alacritty.nix
     ../apps/sway.nix
   ];
+
 
   programs = {
     home-manager.enable = true;
 
-    alacritty = import ../apps/alacritty.nix;
-
     bash = {
       enable = true;
-        profileExtra = ''
-            gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-            gsettings set org.gnome.desktop.interface text-scaling-factor 1.1
-            sway
-        '';
-      };
+      profileExtra = ''
+        gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+        gsettings set org.gnome.desktop.interface text-scaling-factor 1.1
+        sway
+      '';
+    };
 
 	element-desktop = {
       enable = true;
-
 	  settings = {
         default_theme = "dark";
 	  };
 	};
 
 	# For some reason, neovim will not be invoked with the -u flag for the customRC code as non-root users,
-    # which should load our init.lua file.
+    # which should load my init.lua file .
     # This is a workaround which will still load the init.lua file even for non-root users.
-    neovim.enable = true;
-    neovim.extraConfig = ''
-luafile /etc/nixos/apps/neovim/init.lua
-'';
+    neovim = {
+      enable = true;
+      extraConfig = ''
+        luafile /etc/nixos/apps/neovim/init.lua
+      '';
+    };
   };
 }
