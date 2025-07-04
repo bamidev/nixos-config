df7aebeb89b3f7428d50acbba406e99a2eaec76a
Bamidev <bamidev@pm.me>
2025-07-04 15:38:22 +0200

    Use jedi for autocompletion instead of rope

--- apps/neovim.nix
+++ apps/neovim.nix
@@ -67,25 +67,19 @@ in {
               pylsp = {
                 configurationSources = {'flake8'},
                 plugins = {
-                  flake8 = {
+                  flake8 = { enabled = false },
+                  jedi_completion = {
                     enabled = true,
+                    fuzzy = true,
                   },
-                  pydocstyle = {
-                    enabled = true,
-                  },
-                  pyflakes = {
-                    enabled = true,
-                  },
-                  pylint = {
-                    enabled = true, 
-                  },
-                  rope_autoimport = {
-                    enabled = true,
-                  },
-                  rope_completion = {
-                    enabled = true,
-                    eager = false,
-                  }
+                  jedi_definition = { enabled = true },
+                  jedi_hover = { enabled = true },
+                  jedi_references = { enabled = true },
+                  jedi_signature_help = { enabled = true },
+                  jedi_symbols = { enabled = true },
+                  pydocstyle = { enabled = true },
+                  pyflakes = { enabled = true },
+                  pylint = { enabled = true },
                 }
               }
             }
