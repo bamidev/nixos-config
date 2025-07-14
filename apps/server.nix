3792695b05a26f8033b08e3706c2a0ccba3c222a
Bamidev <bamidev@pm.me>
2025-07-14 18:49:26 +0200

    Systemd disable sleep/suspend

--- apps/server.nix
+++ apps/server.nix
@@ -5,5 +5,12 @@
     openssh = {
       enable = true;
     };
+
+    systemd.sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+      AllowHybridSleep=no
+      AllowSuspendThenHibernate=no
+    '';
   };
 }
