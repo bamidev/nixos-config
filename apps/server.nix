dc1aa55c484d6a041d992f016943adf554275a74
Bamidev <bamidev@pm.me>
2025-07-16 10:37:11 +0200

    Disable wifi at night for the nas

--- apps/server.nix
+++ apps/server.nix
@@ -1,5 +1,13 @@
-{ ... }: {
+{ pkgs, ... }: {
   services = {
+    cron = {
+      enable = true;
+      systemCronJobs = [
+        "0 22 * * * root ${pkgs.iproute2}/bin/ip link set wlp2s0 down'"
+        "0 9 * * * root ${pkgs.iproute2}/bin/ip link set wlp2s0 up'"
+      ];
+    };
+
     logind.lidSwitch = "ignore";
 
     openssh = {
