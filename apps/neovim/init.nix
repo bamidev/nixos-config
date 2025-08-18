fa40fc7c6f15620fd52728bcc59c5b7d36c658da
Bamidev <bamidev@pm.me>
2025-08-18 16:15:51 +0200

    Make diagnostic messages in neovim display under lines whenever the window gets small

--- apps/neovim/init.nix
+++ apps/neovim/init.nix
@@ -8,6 +8,7 @@
   vim.o.smartindent = true
   vim.o.tabstop = 4
   vim.o.termguicolors = true
+  vim.o.virtualedit = 'all'
   vim.o.winborder = 'rounded'
   vim.opt.completeopt = { "fuzzy", "menuone", "noinsert", "popup" }
   vim.opt.list = true
@@ -21,15 +22,26 @@
   vim.o.foldenable = true
 
 
-  vim.diagnostic.config({
-    --virtual_lines = true,
-    virtual_text = true,
-  })
-
   -- Map keys
   vim.keymap.set({'n', 'v', 'x'}, ';', ':')
 
 
+  -- Put diagnostic messages under the line of code if the window is not wide enough to show it
+  -- properly.
+  vim.api.nvim_create_autocmd({'BufWinEnter', 'VimResized'}, {
+    callback = function(args)
+      local width = vim.api.nvim_win_get_width(0)
+      local is_small = width <= (vim.o.textwidth + 20)
+      vim.diagnostic.config({
+        virtual_lines = is_small,
+        virtual_text = not is_small,
+      })
+    end
+  })
+
+
   require('plugins') -- Load all plugins
   require('lsp')
+
+
 ''
