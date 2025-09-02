bf59bbfe895eea6ccc59b3699d0070d6ab3aa0bf
Bamidev <bamidev@pm.me>
2025-09-02 10:06:07 +0200

    Fix hardware file for nas device

--- device/nas/hardware.nix
+++ device/nas/hardware.nix
@@ -1,10 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
@@ -27,6 +23,8 @@
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
+
+  hardware.enableRedistributableFirmware = lib.mkForce true;
 
   swapDevices = [ ];
 
