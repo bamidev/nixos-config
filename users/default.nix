de6874775f62891e89903003772ba3673dc35474
Bamidev <bamidev@pm.me>
2025-09-16 12:28:54 +0200

    Remove the neovim configuration within home-manager; it was unnecessary because the environment variable needed to load the system-wide init.lua is now defined

--- users/default.nix
+++ users/default.nix
@@ -3,9 +3,6 @@
     stateVersion = "24.11";
 
     file = {
-      ".config/pylint-default".text = ''
-      '';
-
       ".config/pylintrc" = lib.mkDefault {
         source = /etc/nixos/apps/neovim/etc/pylintrc;
       };
@@ -23,16 +20,6 @@
     git = {
       enable = true;
       package = pkgs.gitFull;
-    };
-
-    # For some reason, neovim will not be invoked with the -u flag for the customRC code as non-root users,
-    # which should load my init.lua file .
-    # This is a workaround which will still load the init.lua file even for non-root users.
-    neovim = {
-      enable = true;
-      extraConfig = ''
-        luafile /etc/xdg/nvim/init.lua
-      '';
     };
   };
 
