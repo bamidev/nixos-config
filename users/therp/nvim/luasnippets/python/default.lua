cfee2303b167d1e4f038ffba3561fd2b891638c3
Bamidev <bamidev@pm.me>
2025-09-21 08:51:32 +0200

    Add some odoo python snippets for user therp

--- users/therp/nvim/luasnippets/python/default.lua
+++ users/therp/nvim/luasnippets/python/default.lua
@@ -7,7 +7,50 @@ local s = luasnip.snippet
 local t = luasnip.text_node
 
 
+local function field_header(type)
+	return {
+		i(1, 'field_name'),
+		t(' = fields.' .. type .. '('),
+	}
+end
+
+local function simple_field(type)
+	local nodes = field_header(type)
+	table.insert(nodes, i(2))
+	table.insert(nodes, t(')'))
+	return nodes
+end
+
+local function bool_param(name)
+	return {
+		t(name .. '='),
+		c(1, {
+			t('True'),
+			t('False'),
+		}),
+	}
+end
+
+local function x2x_field(type)
+	local h = field_header(type)
+	return {
+		h[1],
+		h[2],
+		t('"'),
+		i(2, 'model.name'),
+		t('"'),
+		i(3, ''),
+		t(')')
+	}
+end
+
+
 return {
+	s('bool', simple_field('Boolean')),
+	s('bin', simple_field('Binary')),
+	s('char', simple_field('Char')),
+	s('float', simple_field('Float')),
+	s('int', simple_field('Integer')),
 	s('license', {
 		t({
 			'# ' .. license.LICENSE_HEADER[1],
@@ -15,7 +58,8 @@ return {
 			'', ''
 		}),
 	}),
-
+	s('m2m', x2x_field('Many2many')),
+	s('m2o', x2x_field('Many2one')),
 	s("model-file", {
 		t({
 			'# ' .. license.LICENSE_HEADER[1],
@@ -43,5 +87,12 @@ return {
 			},
 		}),
 		t({'"', '', ''}),
+	}),
+	s('o2m', x2x_field('One2many')),
+	s('read', bool_param('readonly')),
+	s('str', {
+		t('string="'),
+		i(1),
+		t('"'),
 	}),
 }
