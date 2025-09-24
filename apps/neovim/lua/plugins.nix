6f10b5020b5ac72e5d72ab2499d3eecd4c12996e
Bamidev <bamidev@pm.me>
2025-09-24 16:45:19 +0200

    Change keybinding for toggling git blame

--- apps/neovim/lua/plugins.nix
+++ apps/neovim/lua/plugins.nix
@@ -82,7 +82,7 @@
         local gitsigns = require('gitsigns')
         gitsigns.setup({
           on_attach = function()
-            vim.keymap.set('n', '<C-b>', gitsigns.toggle_current_line_blame)
+            vim.keymap.set({'n', 'i'}, '<F10>', gitsigns.toggle_current_line_blame)
           end
         })
       end,
