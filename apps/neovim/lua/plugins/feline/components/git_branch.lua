6a33df060e4d1dbbc94cf053286d8d333b94134f
Bamidev <bamidev@pm.me>
2025-10-10 10:32:15 +0200

    Give the feline component for the git branch a nice icon

--- apps/neovim/lua/plugins/feline/components/git_branch.lua
+++ apps/neovim/lua/plugins/feline/components/git_branch.lua
@@ -1,13 +1,18 @@
 local c = require('plugins.feline.common')
 local git = require('utils.git')
+local utf8 = require('utf8')
+
+
+local branch = c.fancy_component(
+	function()
+		return git.get_branch() or ''
+	end,
+	'oceanblue',
+	'left'
+)
+branch.icon = utf8.char(0xE0A0)
 
 
 return {
-	c.fancy_component(
-		function()
-			return git.get_branch() or ''
-		end,
-		'oceanblue',
-		'left'
-	)
+	branch
 }
