b139fbe71c1b99532d943a61988e8babf54fbb6a
Bamidev <bamidev@pm.me>
2025-10-31 11:04:15 +0100

    Bump the odoo-ls version to 1.0.3

--- users/therp/nvim-init-odoo.nix
+++ users/therp/nvim-init-odoo.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }: ''
+{ pkgs, ... }:
+let
+  version = "1.0.3";
+in ''
   local lsp_dir = vim.fs.abspath('~/lsp')
 
   local odools_dir = lsp_dir .. '/odoo-ls'
   if vim.fn.isdirectory(odools_dir) == 0 then
-    os.execute('git clone -b "1.0.2" --recurse-submodules --depth=1 https://github.com/odoo/odoo-ls.git ' .. odools_dir)
+    os.execute('git clone -b "${version}" --recurse-submodules --depth=1 https://github.com/odoo/odoo-ls.git ' .. odools_dir)
     os.execute('${pkgs.rustup}/bin/rustup install 1.90.0')
     os.execute('cd "' .. odools_dir .. '/server" && ${pkgs.rustup}/bin/rustup run 1.90.0 cargo  build --release')
     os.execute('cp "' .. odools_dir .. '/server/target/release/odoo_ls_server" "' .. odools_dir .. '/server/odoo_ls_server"')
