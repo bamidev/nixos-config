c95fd56c0d6ab2d9ebc9fabb1c8d01b403453be3
Bamidev <bamidev@pm.me>
2025-11-21 14:17:22 +0100

    Fix desktop file for neovim

--- apps/neovim/desktop.nix
+++ apps/neovim/desktop.nix
@@ -1,12 +1,11 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 {
   home.file.".local/share/applications/neovim.desktop".text = ''
     [Desktop Entry]
     Name=Neovim
     GenericName=Text Editor
     Comment=Edit text files
-    TryExec=nvim
-    Exec=$TERMINAL -e nvim %F
+    Exec=${lib.getExe pkgs.alacritty} -e ${lib.getExe config.programs.neovim.package} %F
     Type=Application
     Keywords=Text;editor;
     Icon=nvim
