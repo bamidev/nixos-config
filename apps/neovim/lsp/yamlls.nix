30285643034f767d53fd5521001b1d139a977fbe
Bamidev <bamidev@pm.me>
2025-11-23 09:31:27 +0100

    SPecify filetype for yamlls

--- apps/neovim/lsp/yamlls.nix
+++ apps/neovim/lsp/yamlls.nix
@@ -2,5 +2,6 @@
   vim.lsp.config('yamlls', {
     cmd = {'${pkgs.yaml-language-server}/bin/yaml-language-server', '--stdio'},
     on_attach = require('autocomplete'),
+    filetypes = {'yaml'},
   })
 ''
