1c2ae9620c933eb66866df3b48d81309928cc21a
Bamidev <bamidev@pm.me>
2025-12-05 12:22:28 +0100

    Changed pins so that they take the head of the branches instead of tags, because the branches still get updates

--- pins.nix
+++ pins.nix
@@ -1,16 +1,11 @@
 rec {
   homeManager25_05Src = builtins.fetchTarball
-    "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+    "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.05.zip";
   homeManager25_11Src = builtins.fetchTarball
-    "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+    "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.11.zip";
   homeManager25_05 = import "${homeManager25_05Src}/nixos";
   homeManager25_11 = import "${homeManager25_11Src}/nixos";
   nixpkgs25_05 = import (
-    builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz"
-  ) {};
-  # A very recent source (not actually a pin), for getting pretty up-to-date packages.
-  # Watch out, this may trigger things to be recompiled fairly often.
-  nixpkgsUnstable = import (
-    builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/master.zip"
+    builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-25.05.zip"
   ) {};
 }
