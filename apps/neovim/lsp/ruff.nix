1b9aad85ab26f45f2e313b10c9c42b6c03e8e5ae
Bamidev <bamidev@pm.me>
2026-01-15 13:56:15 +0100

    No autocomplete for Ruff

--- apps/neovim/lsp/ruff.nix
+++ apps/neovim/lsp/ruff.nix
@@ -2,7 +2,6 @@
   vim.lsp.config('ruff', {
     cmd = {'${pkgs.ruff}/bin/ruff', 'server'},
     filetypes = {'python'},
-    on_attach = require('autocomplete'),
     root_markers = {'requirements.txt', '.git'}
   })
 ''
