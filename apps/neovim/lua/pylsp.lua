13e04349a8b68e5419aff416bd547c9b698b3df5
Bamidev <bamidev@pm.me>
2026-01-17 09:36:25 +0100

    Fix/disable some warnings from pylsp

--- apps/neovim/lua/pylsp.lua
+++ apps/neovim/lua/pylsp.lua
@@ -1,7 +1,7 @@
 return {
 	settings = {
 		pylsp = {
-			configurationSources = {"flake8"},
+			configurationSources = {"flake8", "pycodestyle"},
 			plugins = {
 				flake8 = { enabled = true },
 				jedi_completion = {
--- users/therp/nvim/after/lsp/pylsp.lua
+++ users/therp/nvim/after/lsp/pylsp.lua
@@ -6,8 +6,10 @@ end
 
 
 local settings = require('pylsp').settings
+-- Override configurationSource to only pycodestyle, because when using both, the ~/.config/pycodestyle file isn't actually used for some reason.
+settings.pylsp.configurationSources = { "pycodestyle" }
 settings.pylsp.plugins.jedi = { environment = python_path }
--- The following plugins don't know about migration scripts' built-in variables:
+-- The following plugins don't know about migration scripts' built-in variables, so lets disable them:
 settings.pylsp.plugins.flake8.enabled = false
 settings.pylsp.plugins.pyflakes.enabled = false
 -- Disable import errors from pylint, because pylint is not aware of which Wax venv is being used,
@@ -15,10 +17,11 @@ settings.pylsp.plugins.pyflakes.enabled = false
 -- Disable filename checking because migration scripts' filenames require an unconvential file name
 -- style.
 -- Disable function & module docstrings because migration scripts are not expected to have them.
---[[table.insert(
+-- Disable super().member checks because pylsp is not aware of all the members of the odoo base classes.
+table.insert(
 	settings.pylsp.plugins.pylint.args,
-	"--disable=import-error,invalid-name,missing-function-docstring,missing-module-docstring"
-)]]--
+	"--disable=import-error,invalid-name,missing-function-docstring,missing-module-docstring,no-member"
+)
 return {
 	cmd = {'pylsp'},
 	filetypes = {'python'},
