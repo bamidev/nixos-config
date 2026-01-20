be894bb18f34251b8cd5944a1b3aed4be776bc92
Bamidev <bamidev@pm.me>
2026-01-20 08:57:27 +0100

    Define rustup version to install odoo-ls with at the top of the file

--- users/therp/nvim-init-odoo.nix
+++ users/therp/nvim-init-odoo.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 let
   version = "1.0.4";
+  rust_version = "1.91.0";
 in ''
   local lsp_dir = vim.fs.abspath('~/lsp')
 
   local odools_dir = lsp_dir .. '/odoo-ls'
   if vim.fn.isdirectory(odools_dir) == 0 then
     os.execute('git clone -b "${version}" --recurse-submodules --depth=1 https://github.com/odoo/odoo-ls.git ' .. odools_dir)
-    os.execute('${pkgs.rustup}/bin/rustup install 1.91.0')
-    os.execute('cd "' .. odools_dir .. '/server" && ${pkgs.rustup}/bin/rustup run 1.91.0 cargo build --release')
+    os.execute('${pkgs.rustup}/bin/rustup install ${rust_version}')
+    os.execute('cd "' .. odools_dir .. '/server" && ${pkgs.rustup}/bin/rustup run ${rust_version} cargo build --release')
     os.execute('cp "' .. odools_dir .. '/server/target/release/odoo_ls_server" "' .. odools_dir .. '/server/odoo_ls_server"')
 
     os.execute('sleep 10')
