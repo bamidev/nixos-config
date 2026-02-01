ff90050cc2fb0c4a88099ce8ea3b711556007eba
Bamidev <bamidev@pm.me>
2026-02-01 11:07:17 +0100

    Make neovim utilize the rust-analyzer binary from the local flake when available.

--- apps/neovim/lsp/rust_analyzer.nix
+++ apps/neovim/lsp/rust_analyzer.nix
@@ -17,7 +17,7 @@
   end
 
   vim.lsp.config('rust_analyzer', {
-    cmd = { '${pkgs.rust-analyzer}/bin/rust-analyzer' },
+    cmd = { 'rust-analyzer' },
     filetypes = { 'rust' },
     root_dir = function(bufnr, on_dir)
       local fname = vim.api.nvim_buf_get_name(bufnr)
