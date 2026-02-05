ef25aa2b1ea2535d4825003b39a91718f4a36219
Bamidev <bamidev@pm.me>
2026-02-05 10:43:36 +0100

    Improve speed of the feline status line by not running a git command, but taking the branch already loaded from the gitsigns plugin

--- apps/neovim/lua/utils/git.lua
+++ apps/neovim/lua/utils/git.lua
@@ -2,14 +2,7 @@ local this = {}
 
 
 this.get_branch = function()
-	local c = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
-	if c == nil then
-		return nil
-	end
-
-	local output = c:read("*l")
-	c:close()
-	return output
+	return vim.g.gitsigns_head
 end
 
 
