b69976b6a60b86576bd6b8a9e8ad0ec8cccc45ec
Bamidev <bamidev@pm.me>
2026-05-19 10:34:39 +0200

    Disable markdown workaround for now, because it is inteferring with the lsp hints the lsp hints..

--- apps/neovim/ftplugin/markdown.lua
+++ apps/neovim/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
 -- For some reason, I can't get close_fold_kinds_for_ft to work for markdown, but this workaround suffices
-vim.defer_fn(function()
-	require('ufo').closeAllFolds()
-end, 100)
+--vim.defer_fn(function()
+--	require('ufo').closeAllFolds()
+--end, 100)
