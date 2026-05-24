f495a322ad3f5ab71107dcdf2a898d3757fae059
Bamidev <bamidev@pm.me>
2026-05-24 23:20:13 +0200

    Move keymap to toggle inlay hints

--- apps/neovim/lua/lsp.nix
+++ apps/neovim/lua/lsp.nix
@@ -11,9 +11,6 @@
 
   -- Enable sweet inlay hints where available
   vim.lsp.inlay_hint.enable(true)
-  vim.keymap.set('n', '<C-h>', function()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-  end)
 
 
   vim.lsp.enable('bashls')
