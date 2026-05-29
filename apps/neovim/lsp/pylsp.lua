45bbc0b0c0fb8e597e2611d21ab650544f2744e9
Bamidev <bamidev@pm.me>
2026-05-29 17:55:33 +0200

    Show all of pydocstyle diagnostics as hints

--- apps/neovim/lsp/pylsp.lua
+++ apps/neovim/lsp/pylsp.lua
@@ -3,5 +3,17 @@ vim.lsp.config('pylsp', {
 	filetypes = {'python'},
 	settings = require('pylsp').settings,
 	root_markers = {'requirements.txt', '.git'},
-	capabilities = require('lsp.capabilities')
+	capabilities = require('lsp.capabilities'),
+	handlers = {
+          ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+              if result and result.diagnostics then
+                  for _, diag in ipairs(result.diagnostics) do
+                      if diag.source == 'pydocstyle' then
+                          diag.severity = vim.diagnostic.severity.HINT
+                      end
+                  end
+              end
+              vim.lsp.handlers['textDocument/publishDiagnostics'](err, result, ctx, config)
+          end
+      }
 })
