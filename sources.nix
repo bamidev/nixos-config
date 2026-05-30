3a20a001b754cabd9a1ebd0e589b5e9e61f1255b
Bamidev <bamidev@pm.me>
2026-05-30 22:13:59 +0200

    Add 26.05 sources already

--- sources.nix
+++ sources.nix
@@ -3,8 +3,11 @@ rec {
     "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.05.zip";
   homeManager25_11Src = builtins.fetchTarball
     "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.11.zip";
+  homeManager26_05Src = builtins.fetchTarball
+    "https://github.com/nix-community/home-manager/archive/refs/heads/release-26.05.zip";
   homeManager25_05 = import "${homeManager25_05Src}/nixos";
   homeManager25_11 = import "${homeManager25_11Src}/nixos";
+  homeManager26_05 = import "${homeManager26_05Src}/nixos";
   nixpkgs25_05 = import (
     builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-25.05.zip"
   ) {};
