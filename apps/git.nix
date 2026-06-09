59a1094b0a2b9f88d63fdd26c8d1dd792af01f5f
Bamidev <bamidev@pm.me>
2026-06-09 09:09:06 +0200

    Remove duplicate gtk3 configuration

--- apps/git.nix
+++ apps/git.nix
@@ -18,12 +18,6 @@
         [push]
         autoSetupRemote = true
       '';
-
-      "gtk-3.0/settings.ini".text = ''
-        [Settings]
-        gtk-theme-name=Adwaita:dark
-        gtk-application-prefer-dark-theme=1
-      '';
     };
 
     shellAliases = {
