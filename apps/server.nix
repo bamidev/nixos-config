c9eef350c75246881ec525fb2685542860de368c
Bamidev <bamidev@pm.me>
2026-06-14 16:06:44 +0200

    Update server sleep settings

--- apps/server.nix
+++ apps/server.nix
@@ -11,10 +11,10 @@
     logind.lidSwitch = "ignore";
   };
 
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
 }
