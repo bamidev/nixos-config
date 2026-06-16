f84bb3c9462f573823ff70a0756c238cd44ea67f
Bamidev <bamidev@pm.me>
2026-06-16 20:12:00 +0200

    Hide pull error because the home manager activation script is not able to decrypt the ssh key in all scenarios

--- users/bamilab.nix
+++ users/bamilab.nix
@@ -9,7 +9,7 @@
   home.activation.updateExtraConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ -e /etc/xdg/extra-config ]; then
       export GIT_SSH="${pkgs.openssh}/bin/ssh"
-      ${lib.getExe pkgs.git} -C /etc/xdg/extra-config pull
+      ${lib.getExe pkgs.git} -C /etc/xdg/extra-config pull || true
     fi
   '';
 
