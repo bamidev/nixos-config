f2a586ce34d8a9801943f9c3d6d236e326b7ba82
Bamidev <bamidev@pm.me>
2026-06-17 21:30:11 +0200

    Cleanup

--- configuration.nix
+++ configuration.nix
@@ -119,16 +119,8 @@ in
     '';
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
   # Users
   users = {
