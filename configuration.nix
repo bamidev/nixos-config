eac4ad51fd363a3c441d1d4a1dac38a99ae5ec2c
Bamidev <bamidev@pm.me>
2026-07-08 12:09:19 +0200

    /root/.password for all devices

--- configuration.nix
+++ configuration.nix
@@ -191,4 +191,8 @@ in
   };
 
   security.polkit.enable = true;
+
+  # An unaccessible dir for storing plain passwords for when there is no other way to keep things
+  # declarative.
+  systemd.tmpfiles.rules = [ "d /root/.password 1600 root root -" ];
 }
--- lab.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-{
-  # An unaccessible dir for storing plain passwords for when there is no other way to keep things
-  # declarative.
-  systemd.tmpfiles.rules = [ "d /root/.password 1600 root root -" ];
-}
