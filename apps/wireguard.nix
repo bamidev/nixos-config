f7861cfec9ae503b33131b727d6d93241792f2f8
Bamidev <bamidev@pm.me>
2026-07-14 07:10:31 +0200

    Add the home-server to the wireguard peers

--- apps/wireguard.nix
+++ apps/wireguard.nix
@@ -1,16 +1,33 @@
 { config, pkgs, ... }:
-{
-  networking.wg-quick.interfaces.wg0 = {
-    address = [ "10.0.0.${toString config.myvpn.currentDeviceId}/24" ];
-    autostart = true;
-    generatePrivateKeyFile = true;
-    privateKeyFile = "/root/.secret/wireguard-key";
+let
+  port = 51820;
+in {
+  environment.systemPackages = with pkgs; [ wireguard-tools ];
 
-    peers = [
-      {
-        publicKey = "zFi+hWmuEDThYzCZOC8p+u4h9ZuNIkReI81L1ycNHVI=";
-        allowedIPs = [ "10.0.0.0/24" ];
-      }
-    ];
+  networking = {
+    wg-quick.interfaces.wg0 = {
+      address = [ "10.0.0.${toString config.myvpn.currentDeviceId}/24" ];
+      autostart = true;
+      generatePrivateKeyFile = true;
+      privateKeyFile = "/root/.secret/wireguard-key";
+      listenPort = port;
+
+      peers = [
+        # old-laptop1
+        {
+          publicKey = "6GswTjhFuA9xggeiw/1mzHi/DCYGBNUKFi6zd6k19zQ=";
+          allowedIPs = [ "10.0.0.0/24" ];
+          endpoint = "192.168.0.254:${toString port}";
+        }
+        # work-laptop
+        {
+          publicKey = "zFi+hWmuEDThYzCZOC8p+u4h9ZuNIkReI81L1ycNHVI=";
+          allowedIPs = [ "10.0.0.0/24" ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    firewall.allowedUDPPorts = [ port ];
   };
 }
