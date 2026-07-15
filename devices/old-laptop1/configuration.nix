58a411bc89155677afad08d358f581e37c720871
Bamidev <bamidev@pm.me>
2026-07-15 16:20:47 +0200

    Set up a temporary postgresql database on old-laptop1

--- devices/old-laptop1/configuration.nix
+++ devices/old-laptop1/configuration.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   imports = [
     ../../apps/home-server.nix
@@ -12,6 +12,37 @@
     homelab = {
       controlNodeId = 1;
       mainNetworkInterface = "wlp2s0";
+    };
+
+    services.postgresql = {
+      enable = true;
+      package = pkgs.postgresql_17;
+
+      # Temporarily host a Postgres database on old-laptop1, that can be accessed by any kubernetes pod
+      enableTCPIP = true;
+      settings.listen_addresses = "*";
+
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  address         auth-method
+        local all       all                     trust
+        host  all       all     192.168.0.0/16  trust
+      '';
+
+      ensureUsers = [
+        {
+          name = "bamilab";
+          ensureClauses = {
+            createdb = true;
+            login = true;
+          };
+        }
+        {
+          name = "nextcloud";
+          ensureClauses = {
+            login = true;
+          };
+        }
+      ];
     };
   };
 }
