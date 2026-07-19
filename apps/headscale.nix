ad55c3f309ea3041f70ef99f9afd18c524c03897
Bamidev <bamidev@pm.me>
2026-07-19 08:01:08 +0200

    headscale/tailscale fix

--- apps/headscale.nix
+++ apps/headscale.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 let
   username = "bamilab";
+  
+  connectScript = pkgs.writers.writeBashBin "connect-tailscale" (
+    with pkgs;
+    ''
+      set -ex
+      AUTHKEY=$(sudo /run/current-system/sw/bin/headscale preauthkeys create --user 1 --reusable --expiration 24h")
+      ${tailscale}/bin/tailscale up --login-server http://${config.homelab.vps.ip}:8080 --authkey "$AUTHKEY"
+    ''
+  );
 in
 {
   security.sudo = {
@@ -9,7 +18,7 @@ in
       {
         commands = [
           {
-            command = "${pkgs.headscale}/bin/headscale";
+            command = "/run/current-system/sw/bin/headscale";
             options = [ "NOPASSWD" ];
           }
         ];
