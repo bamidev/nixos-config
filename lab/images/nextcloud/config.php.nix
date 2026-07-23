5339eeaf8434c8da9eaa3c2a37b06b5b5567c225
Bamidev <bamidev@pm.me>
2026-07-23 12:10:30 +0200

    Allow reverse proxy access for nextcloud

--- lab/images/nextcloud/config.php.nix
+++ lab/images/nextcloud/config.php.nix
@@ -1,50 +1,53 @@
 { nextcloud }: ''
   <?php
   $CONFIG = [
-  	// General
-  	'instanceid' => 'ocweuq5tkc76',
-  	'passwordsalt' => 'PASSWORD_SALT',
-  	'secret' => 'NEXTCLOUD_SECRET',
-  	'trusted_domains' => [
-  		'nextcloud.kubes',
-  		'192.168.0.77',
-  		'100.64.0.3',
-  	],
+    // General
+    'instanceid' => 'ocweuq5tkc76',
+    'passwordsalt' => 'PASSWORD_SALT',
+    'secret' => 'NEXTCLOUD_SECRET',
+    'trusted_domains' => [
+      'nextcloud.kubes',
+      '192.168.0.77',
+      '100.64.0.3',
+    ],
+    'trusted_proxies' => [
+      '192.168.0.0/24'
+    ],
     'overwrite.cli.url' => 'http://localhost',
-  	'datadirectory' => '/mnt/data',
-  	'version' => '${nextcloud.version}.2',
+    'datadirectory' => '/mnt/data',
+    'version' => '${nextcloud.version}',
 
-  	// Database
-  	'dbtype' => 'pgsql',
-  	'dbhost' => 'POSTGRES_HOST',
-  	'dbname' => 'nextcloud',
-  	'dbuser' => 'nextcloud',
-  	'dbpassword' => 'POSTGRES_PASSWORD',
-  	'dbtableprefix' => 'oc_',
-  	'installed' => true,
+    // Database
+    'dbtype' => 'pgsql',
+    'dbhost' => 'POSTGRES_HOST',
+    'dbname' => 'nextcloud',
+    'dbuser' => 'nextcloud',
+    'dbpassword' => 'POSTGRES_PASSWORD',
+    'dbtableprefix' => 'oc_',
+    'installed' => true,
 
-  	'default_language' => 'en',
-  	'default_timezone' => 'Europe/Amsterdam',
-  	'allow_user_to_change_display_name' => true,
-  	'skeletondirectory' => '/mnt/core/skeleton',
+    'default_language' => 'en',
+    'default_timezone' => 'Europe/Amsterdam',
+    'allow_user_to_change_display_name' => true,
+    'skeletondirectory' => '/mnt/core/skeleton',
 
-  	'apps_paths' => [
-  		[
-  			'path' => '${nextcloud}/apps',
-  			'url' => '/coreapps',
-  			'writable' => false,
-  		],
-  		[
-  			'path' => '/var/nextcloud/apps',
-  			'url' => '/apps',
-  			'writable' => false,
-  		],
-  	],
+    'apps_paths' => [
+      [
+        'path' => '${nextcloud}/apps',
+        'url' => '/coreapps',
+        'writable' => false,
+      ],
+      [
+        'path' => '/var/nextcloud/apps',
+        'url' => '/apps',
+        'writable' => false,
+      ],
+    ],
 
     'log_type' => 'file',
     'logfile' => '/dev/stderr',
     'loglevel' => 1,
-  	'loglevel_frontend' => 1,
-  	'loglevel_dirty_database_queries' => 0,
+    'loglevel_frontend' => 1,
+    'loglevel_dirty_database_queries' => 0,
   ];
 ''
