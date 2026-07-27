c73b9e5f48643ed9b484c97e2260b358f38da828
Bamidev <bamidev@pm.me>
2026-07-27 11:43:33 +0200

    Smaller keepalive interval for wireguard

--- apps/wireguard.nix
+++ apps/wireguard.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 let
+  homeKeepaliveInterval = 10;
   port = 51820;
 in
 {
@@ -31,25 +32,25 @@ in
           {
             publicKey = "6GswTjhFuA9xggeiw/1mzHi/DCYGBNUKFi6zd6k19zQ=";
             allowedIPs = [ "172.0.0.10/32" ];
-            persistentKeepalive = 25;
+            persistentKeepalive = homeKeepaliveInterval;
           }
           # old-laptop-asus
           {
             publicKey = "Biu59vkCyQnkOgMP88m8hLZf6yxTuM7CAjbnmTRPZHY=";
             allowedIPs = [ "172.0.0.11/32" ];
-            persistentKeepalive = 25;
+            persistentKeepalive = homeKeepaliveInterval;
           }
           # thinkcentre
           {
             publicKey = "rcizmz3S2CU2+7ElqLeWepozVWct5/UB75gOZ9zxty4=";
             allowedIPs = [ "172.0.0.12/32" ];
-            persistentKeepalive = 25;
+            persistentKeepalive = homeKeepaliveInterval;
           }
           # work-laptop
           {
             publicKey = "zFi+hWmuEDThYzCZOC8p+u4h9ZuNIkReI81L1ycNHVI=";
             allowedIPs = [ "172.0.0.1/32" ];
-            persistentKeepalive = 25;
+            persistentKeepalive = homeKeepaliveInterval;
           }
         ];
       };
