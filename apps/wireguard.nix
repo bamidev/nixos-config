7f077ff2828d4935ec95c453e1dc2c51df120c1f
Bamidev <bamidev@pm.me>
2026-07-27 20:43:23 +0200

    FIx the allowedIPs in wg

--- apps/wireguard.nix
+++ apps/wireguard.nix
@@ -30,31 +30,31 @@ in
           # vps
           {
             publicKey = "1U1LwQYOeOT1HOGAtWSOfxPy6055tG8/xOb2wcnXskY=";
-            allowedIPs = [ "172.0.0.100/32" ];
+            allowedIPs = [ "172.0.0.0/24" ];
             endpoint = "${config.homelab.vps.ip}:${toString port}";
           }
           # old-laptop-msi
           {
             publicKey = "6GswTjhFuA9xggeiw/1mzHi/DCYGBNUKFi6zd6k19zQ=";
-            allowedIPs = [ "172.0.0.10/32" ];
+            allowedIPs = [ "172.0.0.0/24" ];
             persistentKeepalive = homeKeepaliveInterval;
           }
           # old-laptop-asus
           {
             publicKey = "Biu59vkCyQnkOgMP88m8hLZf6yxTuM7CAjbnmTRPZHY=";
-            allowedIPs = [ "172.0.0.11/32" ];
+            allowedIPs = [ "172.0.0.0/24" ];
             persistentKeepalive = homeKeepaliveInterval;
           }
           # thinkcentre
           {
             publicKey = "rcizmz3S2CU2+7ElqLeWepozVWct5/UB75gOZ9zxty4=";
-            allowedIPs = [ "172.0.0.12/32" ];
+            allowedIPs = [ "172.0.0.0/24" ];
             persistentKeepalive = homeKeepaliveInterval;
           }
           # work-laptop
           {
             publicKey = "zFi+hWmuEDThYzCZOC8p+u4h9ZuNIkReI81L1ycNHVI=";
-            allowedIPs = [ "172.0.0.1/32" ];
+            allowedIPs = [ "172.0.0.0/24" ];
             persistentKeepalive = homeKeepaliveInterval;
           }
         ];
