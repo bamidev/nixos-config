d8ac39a26b8504a049f966a91aa8edf646a4b2b3
Bamidev <bamidev@pm.me>
2026-07-27 21:43:28 +0200

    Only ping 4 times

--- devices/wifi.nix
+++ devices/wifi.nix
@@ -7,7 +7,7 @@
       systemCronJobs = [
         "0  22 * * * root ${pkgs.util-linux}/bin/rfkill block wifi"
         "0  9  * * * root ${pkgs.util-linux}/bin/rfkill unblock wifi"
-        "10 9  * * * root ${pkgs.iputils}/bin/ping 172.0.0.100"
+        "10 9  * * * root ${pkgs.iputils}/bin/ping -c 4 172.0.0.100"
       ];
     };
   };
