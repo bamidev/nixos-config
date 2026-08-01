2c89d4fc33301ec22bcea0b330d2bd06d058187a
Bamidev <bamidev@pm.me>
2026-08-01 11:08:02 +0200

    Only enable the floating ip address to one device for the moment, to prevent issues I will debug later

--- devices/thinkcentre/configuration.nix
+++ devices/thinkcentre/configuration.nix
@@ -9,6 +9,7 @@
     controlNodeId = 2;
     mainNetworkInterface = "enp1s0f1";
     deviceZpool = "main";
+    enableKeepalived = false; # Don't use VRRP for this device ATM because the IP address is not being correctly assigned which causes issues.
   };
 
   homevpn.deviceId = 12;
