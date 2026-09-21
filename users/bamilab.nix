777293775ec339fa008347823d4cdb2b4fc41fcd
Bamidev <bamidev@pm.me>
2026-09-21 14:23:11 +0200

    Add the webdav mount for my music and ambience files to the gtk bookmarks, and remove the old mount of the Music folder in Nextcloud

--- users/bamilab.nix
+++ users/bamilab.nix
@@ -24,7 +24,7 @@ in
     file = {
       ".config/gtk-3.0/bookmarks".text = ''
         dav://nextcloud.local.bamilab.space/remote.php/dav/files/bamilab/
-        dav://nextcloud.bamilab.space/remote.php/dav/files/bamilab/Music
+        dav://music.bamilab.space/webdav
       '';
 
       ".config/quodlibet/default-config".text = defaultQuodLibetConfig;
